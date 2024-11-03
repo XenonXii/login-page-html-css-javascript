0ef352fe4f8913fa74f50bb03f3bc71242da9255
XenonXii <xenonxii00@gmail.com>
2024-11-03 14:46:40 +0100

    3rd commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function get(id){
+     document.getElementById(id);
+
+}
+cl("DEBUG------")
+function cl(x){
+    console.log(x)
+}
+cl("DEBUG------")
+
+// get("login_button").addEventListener("click" , function(){
+//     cl("DEBUG------");
+//     get("login_button").setAttribute("class", "after_click_login_button")
+// })
+function translate_left(){
+cl("DEBUG------")
+
+    document.getElementById("login_button").setAttribute("class","after_click_login_button")
+    document.getElementById("cancel_button").setAttribute("class","after_click_cancel_button")
+}
+function translate_right(){
+    cl("DEBUG------")
+
+    document.getElementById("login_button").setAttribute("class","login_button")
+    document.getElementById("cancel_button").setAttribute("class","cancel_button")
+}
